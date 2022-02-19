a3af62f91ec599f481237d46c02add112de19bde
Josh Bolipata <joshtiffton@gmail.com>
2022-02-19 13:38:06 +0800

    added more info to display

--- lib.js
+++ lib.js
@@ -1,31 +1,32 @@
 const container = document.querySelector('.container');
 
-let myLibrary = [];
+const myLibrary = [];
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function Book(author, title) {
-  this.author = author;
+function Book(author, title, pages) {
   this.title = title;
-  this.addtolib = function () {
-    myLibrary.push(title);
-  }
+  this.author = author;
+  this.pages = pages + ' pages';
 }
 
 function update(library) {
   for (i = 0; i < library.length; i++) {
     let div = document.createElement('div');
-    div.textContent = library[i];
+    for (const property in library[i]) {
+      let content = document.createTextNode(`${library[i][property]}\n`);
+      div.appendChild(content);
+    }
     container.appendChild(div);
   }
 }
 
-const HP = new Book('J.K. Rowling', "Harry Potter and the Philosopher's Stone");
-HP.addtolib();
-const LOTR = new Book('J.R.R. Tolkien', "The Lord of the Rings");
-LOTR.addtolib();
+const HP = new Book('J.K. Rowling', "Harry Potter and the Philosopher's Stone", 500);
+addBookToLibrary(HP);
+const LOTR = new Book('J.R.R. Tolkien', "The Lord of the Rings", 1000);
+addBookToLibrary(LOTR);
 
 update(myLibrary);
 
