5df4e1ffc7dbec0caef44c725436898d771df703
Josh Bolipata <joshtiffton@gmail.com>
2022-02-22 10:02:15 +0800

    Added 'New Book' function

--- lib.js
+++ lib.js
@@ -23,11 +23,35 @@ function update(library) {
   }
 }
 
-const HP = new Book('J.K. Rowling', "Harry Potter and the Philosopher's Stone", 500);
-addBookToLibrary(HP);
-const LOTR = new Book('J.R.R. Tolkien', "The Lord of the Rings", 1000);
-addBookToLibrary(LOTR);
-
+myLibrary['HP'] = new Book('J.K. Rowling', "Harry Potter and the Philosopher's Stone", 500);
+addBookToLibrary(myLibrary['HP']);
+myLibrary['LOTR'] = new Book('J.R.R. Tolkien', "The Lord of the Rings", 1000);
+addBookToLibrary(myLibrary['LOTR']);
 update(myLibrary);
+
+const newbookform = document.querySelector('.newbookform');
+const newbook = document.querySelector('.newbook');
+newbook.addEventListener(('click'), () => {
+  if (newbookform.style.display === "none") {
+    newbookform.style.display= "block";
+  }
+  else {
+    newbookform.style.display= "none";
+  }
+  
+})
+
+function newbookfunction() {
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let varname = `${title.split(" ").join("")}`;
+  myLibrary[`${varname}`] = new Book(title, author, pages);
+  addBookToLibrary(myLibrary[`${varname}`]);
+  container.innerHTML = "";
+  update(myLibrary);
+}
+
+
 
 
